a04e1fcfd72d72bca0e4f7a5e82bfd3a024ec146
Małgorzata Kapuścińska <malgorzata.kapuscinska@intersynergy.pl>
2019-11-07 12:13:59 +0100

    Remove unnecessary files blocking compilation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,6 @@
 import './_config'
 import './_icons'
-import './components/equal-height'
-import './components/header'
-import './components/links-list'
-import './components/product-card'
-import './components/progess-bar'
-import './components/slider'
 
 /* DEV-START */
-import './components/testVueChild'
-import './components/testVueParent'
+
 /* DEV-END */
